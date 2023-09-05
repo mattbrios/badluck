6ea902c2aa6c83952f11cfc856784df9296d82e4
mattbrios <mattbrios@gmail.com>
2023-09-04 23:42:04 -0300

    Add algumas meta tags e icones

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,22 @@
 import '@/styles/globals.css'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Head>
+    <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, shrink-to-fit=no" />
+        <meta name="title" content = "Bad Luck" />
+        <meta name="description" content = "Game Bad Luck. Este game foi desenvolvido para você perder. Sério, você sempre perde." />
+        <meta property="og:title" content="Bad Luck" />
+        <meta property="og:description" content="Game Bad Luck. Este game foi desenvolvido para você perder. Sério, você sempre perde." />
+        <meta property="og:image" content="/og.png" />
+        <meta property='og:type' content='website' />
+        <meta property='og:site_name' content='Bad Luck' />
+        <link rel='canonical' href='https://badluck.mateusrios.com.br' />
+    </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
