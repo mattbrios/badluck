95392246ec2ccadf4b031d6f5f51ce1f5202d657
mattbrios <mattbrios@gmail.com>
2023-09-04 23:05:52 -0300

    Add package hotjar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,22 @@
-import { Sono } from 'next/font/google'
-import HomeComponent from './home'
+import { Sono } from 'next/font/google';
+import Hotjar from '@hotjar/browser';
+import HomeComponent from './home';
+import { useEffect } from 'react';
 
-const sono = Sono({ subsets: ['latin'] })
+const sono = Sono({ subsets: ['latin'] });
+const siteId = 3640567;
+const hotjarVersion = 6;
 
 export default function Home() {
+  useEffect(() => {
+    let mounted = true;
+    if(mounted) {
+      Hotjar.init(siteId, hotjarVersion);
+    }
+  
+    return () => mounted = false;
+  }, []);
+  
   return (
     <main className={`flex min-h-screen flex-col items-center justify-center p-8 ${sono.className}`}>
       <HomeComponent />
