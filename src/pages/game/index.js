f2d3fec019d5c185edff7b85da6d0cc655d531ed
mattbrios <mattbrios@gmail.com>
2023-09-05 21:49:45 -0300

    add event para iniciar a gravação do hotjar

--- src/pages/game/index.js
+++ src/pages/game/index.js
@@ -3,6 +3,8 @@ import dynamic from 'next/dynamic';
 import { Sono } from 'next/font/google'
 import ListPrizes from '@/components/Game/ListPrizes';
 import Head from 'next/head';
+import { Button } from '@/components/Button';
+import Hotjar from '@hotjar/browser';
 
 const sono = Sono({ subsets: ['latin'] })
 
@@ -13,7 +15,12 @@ const DynamicComponent = dynamic ( () =>
 );
 export default function Game () {
   const [ listRaffledPrizes, setListRaffledPrizes ] = useState([]);
+  const [ showRecordButton, setShowRecordButton ] = useState(true);
 
+  const handleStartRecord = () => {
+    setShowRecordButton(false);
+    Hotjar.event('init_record');
+  }
   return (
     <>
       <Head>
@@ -22,6 +29,11 @@ export default function Game () {
       <section className={`flex min-h-screen flex-col items-center p-4 ${sono.className}`}>
         <h1 className='text-xl uppercase font-bold max-w-sm text-center'>Você deve me enviar um <span className='text-yellow-200'>PIX</span> ao finalizar este game</h1>
         <p className='max-w-sm text-center my-4 opacity-50 text-xs'>Gire quantas vezes quiser, acumule, some e envie</p>
+        {showRecordButton && (
+          <div className='mt-2 mb-4'>
+            <Button onClick={handleStartRecord}>Gravar agora</Button>
+          </div>
+        )}
         <DynamicComponent setList={setListRaffledPrizes} />
         {listRaffledPrizes.length > 0 && (
           <ListPrizes list={listRaffledPrizes} />
